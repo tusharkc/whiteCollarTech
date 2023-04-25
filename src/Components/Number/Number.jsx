8177f641a2db659b2192d5e9b1ae5cc9f81dea4e
Tushar Karamchandani <tusharkaramchandani@Tushars-MacBook-Air.local>
2023-04-26 00:24:40 +0530

    added number animation

---
+++ src/Components/Number/Number.jsx
@@ -0,0 +1,14 @@
+import { useSpring, animated } from "@react-spring/web";
+import React from "react";
+
+const Number = ({ n }) => {
+  const { number } = useSpring({
+    from: { number: 0 },
+    number: n,
+    delay: 200,
+    config: { mass: 1, tension: 20, friction: 10 },
+  });
+  return <animated.span>{number.to((n) => n.toFixed(0))}</animated.span>;
+};
+
+export default Number;
--- src/features/HomePage/Components/WCAchivement/WCAchivment.jsx
+++ src/features/HomePage/Components/WCAchivement/WCAchivment.jsx
@@ -1,45 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import wcAchivment from "../../../../assets/images/wcAchivement.png";
+import { Number } from "../../../../Components";
+import { Waypoint } from "react-waypoint";
 
 const WCAchivment = () => {
+  const [inView, setInView] = useState(false);
+
   return (
-    <div className="py-[150px] px-[140px]">
-      <div className="flex items-center justify-around">
-        <div className="max-w-[572px]">
-          <h3 className="text-5xl font-light">
-            A for Awesome A for Achievements
-          </h3>
-          <p className="mt-5">
-            We have been providing technical consulting services for over a
-            decade now and it has been a growing journey for us. Along the way,
-            we've built relationships, onboarded clients, and achieved many
-            milestones.
-          </p>
-          <div className="flex item-center justify-between mt-8">
-            {/* <div className="w-24">
+    <>
+      <Waypoint onEnter={() => setInView(true)} />
+      <div className="py-[150px] px-[140px]">
+        <div className="flex items-center justify-around">
+          <div className="max-w-[572px]">
+            <h3 className="text-5xl font-light">
+              A for Awesome A for Achievements
+            </h3>
+            <p className="mt-5">
+              We have been providing technical consulting services for over a
+              decade now and it has been a growing journey for us. Along the
+              way, we've built relationships, onboarded clients, and achieved
+              many milestones.
+            </p>
+            <div className="flex item-center justify-between mt-8">
+              {/* <div className="w-24">
               <h2 className="text-5xl font-semibold text-[#0055A5]">10+</h2>
               <p>managed projects</p>
             </div> */}
-            <div className="w-24">
-              <h2 className="text-5xl font-semibold text-[#0055A5]">12+</h2>
-              <p>years of experience</p>
-            </div>
-            <div className="w-24">
-              <h2 className="text-5xl font-semibold text-[#0055A5]">1000+</h2>
-              <p>resources placed</p>
-            </div>
-            <div className="w-24">
-              <h2 className="text-5xl font-semibold text-[#0055A5]">100+</h2>
-              <p>happy clients</p>
+
+              {inView ? (
+                <>
+                  <div className="w-24">
+                    <h2 className="text-5xl font-semibold text-[#0055A5]">
+                      <Number n={12} />+
+                    </h2>
+                    <p>years of experience</p>
+                  </div>
+                  <div className="w-24">
+                    <h2 className="text-5xl font-semibold text-[#0055A5]">
+                      <Number n={1000} />+
+                    </h2>
+                    <p>resources placed</p>
+                  </div>
+                  <div className="w-24">
+                    <h2 className="text-5xl font-semibold text-[#0055A5]">
+                      <Number n={100} />+
+                    </h2>
+                    <p>happy clients</p>
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
             </div>
           </div>
+          <img
+            // className="h-[400px] w-[474px] object-cover rounded-2xl"
+            src={wcAchivment}
+          />
         </div>
-        <img
-          // className="h-[400px] w-[474px] object-cover rounded-2xl"
-          src={wcAchivment}
-        />
       </div>
-    </div>
+    </>
   );
 };
 
