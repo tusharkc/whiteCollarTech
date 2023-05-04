5300cbb09870ad685c7111e9748d2d45080106c1
Tushar Karamchandani <tusharkaramchandani@Tushars-MacBook-Air.local>
2023-05-04 21:51:40 +0530

    added changes

--- src/features/HomePage/Components/HomePageCTA/HomePageCTA.jsx
+++ src/features/HomePage/Components/HomePageCTA/HomePageCTA.jsx
@@ -18,53 +18,65 @@ const HomePageCTA = () => {
         </p>
 
         <form>
-          <div>
-            <div className="flex items-center gap-4 mt-16">
+          <div className="grid grid-cols-12 gap-4 mt-16">
+            <input
+              className="col-span-6 outline-none border-b-2 border-black p-1 bg-transparent"
+              placeholder="Full Name"
+              name="fullName"
+              id="fullName"
+            />
+            <input
+              className="col-span-6 outline-none border-b-2 border-black p-1 bg-transparent"
+              placeholder="Phone Number"
+              name="phoneNumber"
+              id="phoneNumber"
+            />
+          </div>
+          {/* <div className="grid-cols-12 bg-red-300 w-full gap-4">
+            <input
+              name="fullName"
+              id="fullName"
+              placeholder="Full Name"
+              className="active:outline-none focus:outline-none p-2 border-b-2 border-black col-span-6"
+            />
+            <input
+              name="phoneNumber"
+              id="phoneNumber"
+              placeholder="Phone Number"
+              className="active:outline-none focus:outline-none p-2 border-b-2 border-black col-span-6"
+            />
+          </div> */}
+
+          <input
+            name="email"
+            id="email"
+            placeholder="Email Address"
+            className="my-20 active:outline-none focus:outline-none border-b-2 border-black p-2 min-w-full"
+          />
+
+          <input
+            name="message"
+            id="message"
+            placeholder="Message (Optional)"
+            className="active:outline-none focus:outline-none border-b-2 border-black min-w-full"
+          />
+
+          <label className="flex items-start justify-center gap-2 mt-14">
+            <div>
               <input
-                name="fullName"
-                id="fullName"
-                placeholder="Full Name"
-                className="active:outline-none focus:outline-none p-2 border-b-2 border-black max-w-[49%]"
-              />
-              <input
-                name="phoneNumber"
-                id="phoneNumber"
-                placeholder="Phonenumber"
-                className="active:outline-none focus:outline-none p-2 border-b-2 border-black max-w-[49%]"
+                style={{ height: "25px", width: "25px" }}
+                type="checkbox"
               />
             </div>
+            <span className="text-[#9F9F9F] font-light ">
+              I have read and agree with White Collar’s stated Privacy Policy
+              and Terms Conditions
+            </span>
+          </label>
 
-            <input
-              name="email"
-              id="email"
-              placeholder="Email Address"
-              className="my-20 active:outline-none focus:outline-none border-b-2 border-black p-2 min-w-full"
-            />
-
-            <input
-              name="message"
-              id="message"
-              placeholder="Message (Optional)"
-              className="active:outline-none focus:outline-none border-b-2 border-black min-w-full"
-            />
-
-            <label className="flex items-start justify-center gap-2 mt-14">
-              <div>
-                <input
-                  style={{ height: "25px", width: "25px" }}
-                  type="checkbox"
-                />
-              </div>
-              <span className="text-[#9F9F9F] font-light ">
-                I have read and agree with White Collar’s stated Privacy Policy
-                and Terms Conditions
-              </span>
-            </label>
-
-            <button className="my-12 bg-[#0055A5] text-white hover:bg-[#141414] px-4 py-2 rounded-lg transition-all">
-              Submit
-            </button>
-          </div>
+          <button className="my-12 bg-[#0055A5] text-white hover:bg-[#141414] px-4 py-2 rounded-lg transition-all">
+            Submit
+          </button>
         </form>
       </div>
     </div>
