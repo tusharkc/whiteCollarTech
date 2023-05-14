0b13c61eac9a2c49ccac5eb542a3f61bc22f55dd
Tushar Karamchandani <tusharkaramchandani@Tushars-MacBook-Air.local>
2023-05-14 16:35:29 +0530

    added change

---
+++ src/data/homePageBannerData.js
@@ -0,0 +1,29 @@
+import homePageHeader1 from "../assets/images/header1.png";
+import homePageHeader2 from "../assets/images/header2.png";
+import homePageHeader3 from "../assets/images/header3.png";
+
+const homepageBannerData = [
+  {
+    heading: "Conjuring up the top IT talent for your business!",
+    subHeading:
+      "We are here to harness the power of top IT talent and help you overcome your challenges.",
+    btnText: "Excel with us",
+    homePageHeaderImg: homePageHeader1,
+  },
+  {
+    heading: "Building your Dream Team and orchestrating excellence!",
+    subHeading:
+      "We source, screen, and assemble the most exceptional talent for you to manage your challenges with finesse.",
+    btnText: "Connect with us",
+    homePageHeaderImg: homePageHeader2,
+  },
+  {
+    heading: "Fuel your life science discoveries and IT needs with us!",
+    subHeading:
+      "When it comes to lab IT, clinical IT, and quality compliance work, we hold the expertise, experience, and leadership!",
+    btnText: "Hire the Experts",
+    homePageHeaderImg: homePageHeader3,
+  },
+];
+
+export default homepageBannerData;
--- src/features/HomePage/Components/HomePageHeader/HomePageHeader.jsx
+++ src/features/HomePage/Components/HomePageHeader/HomePageHeader.jsx
@@ -1,8 +1,7 @@
 import React, { useRef } from "react";
-import homePageHeader1 from "../../../../assets/images/header1.png";
-import homePageHeader2 from "../../../../assets/images/header2.png";
-import homePageHeader3 from "../../../../assets/images/header3.png";
+
 import arrowIconWhite from "../../../../assets/icons/arrowIconWhite.svg";
+import homepageBannerData from "../../../../data/homePageBannerData";
 
 const HomePageHeader = () => {
   const ref = useRef(null);
@@ -11,156 +10,82 @@ const HomePageHeader = () => {
     ref.current.scrollLeft += scrollOffset;
   };
 
+  const HomePageBanner = ({
+    homePageHeaderImg,
+    heading,
+    subHeading,
+    btnText,
+    currentIndex,
+    finalIndex,
+  }) => {
+    return (
+      <>
+        <div
+          style={{
+            backgroundImage: `url(${homePageHeaderImg})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            objectFit: "cover",
+          }}
+          onClick={() => scroll(window.innerWidth)}
+          className="h-screen min-w-full relative"
+        >
+          <div className="max-w-[580px] absolute bottom-[100px] left-[140px]">
+            <h1 className="text-white text-6xl font-extrabold tracking-wider leading-snug">
+              {heading}
+            </h1>
+            <p className="text-white mt-5 mb-10 leading-snug tracking-wider">
+              {subHeading}
+            </p>
+
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              className="bg-white py-3 px-6 rounded-lg hover:bg-[#0055A5] hover:text-white transition-all"
+            >
+              {btnText}
+            </button>
+
+            <div className="text-white flex items-center mt-14">
+              <img
+                onClick={(e) => {
+                  e.stopPropagation();
+                  scroll(-window.innerWidth);
+                }}
+                src={arrowIconWhite}
+                alt=""
+                className="rotate-180"
+              />
+              <p>
+                &nbsp;&nbsp; {currentIndex}/{finalIndex} &nbsp;&nbsp;
+              </p>
+              <img
+                onClick={(e) => {
+                  e.stopPropagation();
+                  scroll(window.innerWidth);
+                }}
+                src={arrowIconWhite}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <div ref={ref} className="flex overflow-x-hidden">
-        <div
-          style={{
-            backgroundImage: `url(${homePageHeader1})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            objectFit: "cover",
-          }}
-          onClick={() => scroll(window.innerWidth)}
-          className="h-screen min-w-full flex items-center px-[140px]"
-        >
-          <div className="max-w-xl">
-            <h1 className="text-white text-6xl font-extrabold tracking-wider leading-tight">
-              Conjuring up the top IT talent for your business!
-            </h1>
-            <p className="text-white mt-5 mb-10">
-              We are here to harness the power of top IT talent and help you
-              overcome your challenges.
-            </p>
-
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-              className="bg-white p-3 rounded-lg hover:bg-[#0055A5] hover:text-white transition-all"
-            >
-              Excel with us
-            </button>
-
-            <div className="text-white flex items-center mt-14">
-              {/* <img
-                onClick={() => scroll(-window.innerWidth)}
-                src={arrowIconWhite}
-                alt=""
-                className="rotate-180"
-              /> */}
-              <p>&nbsp;&nbsp; 1/3 &nbsp;&nbsp;</p>
-              <img
-                onClick={(e) => {
-                  e.stopPropagation();
-                  scroll(window.innerWidth);
-                }}
-                src={arrowIconWhite}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-
-        <div
-          style={{
-            backgroundImage: `url(${homePageHeader2})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            objectFit: "cover",
-          }}
-          onClick={() => scroll(window.innerWidth)}
-          className="h-screen min-w-full flex items-center px-[140px]"
-        >
-          <div className="max-w-2xl">
-            <h1 className="text-white text-6xl font-extrabold tracking-wider leading-tight">
-              Building your IT Dream Team and orchestrating excellence!
-            </h1>
-            <p className="text-white mt-5 mb-10">
-              We source, screen, and assemble the most exceptional <br /> talent
-              for you to conquer your challenges with finesse.
-            </p>
-
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-              className="bg-white p-3 rounded-lg hover:bg-[#0055A5] hover:text-white transition-all"
-            >
-              Connect with us
-            </button>
-
-            <div className="text-white flex items-center mt-14">
-              <img
-                onClick={(e) => {
-                  e.stopPropagation();
-                  scroll(-window.innerWidth);
-                }}
-                src={arrowIconWhite}
-                alt=""
-                className="rotate-180"
-              />
-              <p>&nbsp;&nbsp; 2/3 &nbsp;&nbsp;</p>
-              <img
-                src={arrowIconWhite}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  scroll(window.innerWidth);
-                }}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-
-        <div
-          style={{
-            backgroundImage: `url(${homePageHeader3})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            objectFit: "cover",
-          }}
-          // onClick={() => scroll(-(2 * window.innerWidth))}
-          className="h-screen min-w-full flex items-center px-[140px]"
-        >
-          <div className="max-w-2xl">
-            <h1 className="text-white text-6xl font-extrabold tracking-wider leading-tight">
-              When it comes to Life Sciences and <br /> IT needs, we are
-              <br /> the one!
-            </h1>
-            <p className="text-white mt-5 mb-10">
-              Fuel your scientific discovery with us and pave the way <br /> to
-              your success because we specialize in life sciences.
-            </p>
-
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-              className="bg-white p-3 rounded-lg hover:bg-[#0055A5] hover:text-white transition-all"
-            >
-              Hire the Experts
-            </button>
-
-            <div className="text-white flex items-center mt-14">
-              <img
-                onClick={(e) => {
-                  e.stopPropagation();
-                  scroll(-window.innerWidth);
-                }}
-                src={arrowIconWhite}
-                alt=""
-                className="rotate-180"
-              />
-              <p>&nbsp;&nbsp; 3/3 &nbsp;&nbsp;</p>
-              {/* <img
-                src={arrowIconWhite}
-                onClick={() => scroll(-(2 * window.innerWidth))}
-                alt=""
-              /> */}
-            </div>
-          </div>
-        </div>
+        {homepageBannerData.map((bannerItem, index) => (
+          <HomePageBanner
+            key={index}
+            {...bannerItem}
+            currentIndex={index + 1}
+            finalIndex={homepageBannerData.length}
+          />
+        ))}
       </div>
     </>
   );
