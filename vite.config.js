bc1d2d0b240508fd1249c111a1f84c725bfcf296
Tushar Karamchandani <tusharkaramchandani25@gmail.com>
2024-02-25 18:57:40 +0530

    added multi env set up

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+  };
 });
